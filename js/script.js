d6271c6500814260448134150d9cc610577ff405
Jhonatan Isaac Galiano Guerrero <jhoisagg@Jhonatans-MacBook-Pro.local>
2026-02-21 10:06:13 -0600

    Responsividad

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ function mostrarJuego() {
   document.getElementById("contenedorJuego").style.display = "flex";
   document.getElementById("btnJuego").style.display = "none";
 }
+
+
+const toggleButton = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('.navegacion-principal');
+
+    toggleButton.addEventListener('click', () => {
+        nav.classList.toggle('activo');
+    });
